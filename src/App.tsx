c99287ece2b4e390e09480e5c42e8162460854dc
AndyLab9596 <79738559+AndyLab9596@users.noreply.github.com>
2021-10-31 15:21:54 +0700

    update add fnc logic

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { useQuery } from "react-query";
 import { Drawer, LinearProgress, Grid, Badge } from "@material-ui/core";
 import AddShoppingCart from "@material-ui/icons/AddShoppingCart";
 import Item from "./Item/Item";
+import Cart from "./Cart/Cart";
 // Styles
 import { StyledButton, Wrapper } from "./App.styles";
 
@@ -33,7 +34,23 @@ const App = () => {
   const getTotalItems = (items: CartItemType[]) =>
     items.reduce((ack: number, item) => ack + item.amount, 0);
 
-  const handleAddToCart = (clickedItem: CartItemType) => null;
+  const handleAddToCart = (clickedItem: CartItemType) => {
+    setCartItems((prev) => {
+      // 1. is the item already added in the cart?
+      const isItemInCart = prev.find((item) => item.id === clickedItem.id);
+
+      if (isItemInCart) {
+        return prev.map((item) =>
+          item.id === clickedItem.id
+            ? { ...item, amount: item.amount + 1 }
+            : item
+        );
+      }
+
+      // First time the item is added
+      return [...prev, { ...clickedItem, amount: 1 }];
+    });
+  };
 
   const handleRemoveFromCart = () => null;
 
@@ -43,7 +60,11 @@ const App = () => {
   return (
     <Wrapper>
       <Drawer anchor="right" open={cartOpen} onClose={() => setCartOpen(false)}>
-        Cart goes here
+        <Cart
+          cartItems={cartItems}
+          addToCart={handleAddToCart}
+          removeFromCart={handleRemoveFromCart}
+        />
       </Drawer>
       <StyledButton onClick={() => setCartOpen(true)}>
         <Badge badgeContent={getTotalItems(cartItems)} color="error">
